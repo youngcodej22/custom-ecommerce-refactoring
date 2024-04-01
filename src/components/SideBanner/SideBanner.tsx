ffa28ae837d548eab4b240d1b31dbaec35c359c2
YeongBaeJi <youngcodej22@gmail.com>
2024-04-01 15:54:19 +0900

    ✨feat: UPDATE SideBanner a를 Link로 변경
    Detail 버튼 수정

--- src/components/SideBanner/SideBanner.tsx
+++ src/components/SideBanner/SideBanner.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import SideBannerBrand from '/assets/banner/banner-brand.png';
 import SideBannerStore from '/assets/banner/banner-store.png';
 
@@ -12,19 +14,21 @@ const SideBanner = () => {
                 <div className="txt-wrap">
                     <h2 className="main-tit en">BRAND STORY</h2>
                     <div className="text">MCKAYSON. est. 1806</div>
-                    <a href="" className="detail-btn">
+                    <Link to="/brand" className="detail-btn">
                         DETAIL &nbsp;&nbsp; &nbsp;+
-                    </a>
+                    </Link>
                 </div>
             </div>
             <div className="store-wrap banner-cont">
                 <img src={SideBannerStore} alt="클랜맥케이" />
                 <div className="txt-wrap">
                     <h2 className="main-tit en">CLAN MACKAY</h2>
-                    <div className="text">맥케이슨이 클랜 맥케이를 응원합니다</div>
-                    <a href="" className="detail-btn">
+                    <div className="text">
+                        맥케이슨이 클랜 맥케이를 응원합니다
+                    </div>
+                    <Link to="/brand" className="detail-btn">
                         DETAIL &nbsp;&nbsp; &nbsp;+
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
