2877fb97d25cbbb6616769a5a49fc9ff8faf7bb1
YeongBaeJi <youngcodej22@gmail.com>
2024-05-21 18:51:03 +0900

    🎨style: UPDATE ProductListFilterDetail 가격 slider UI 변경 / 📦assets: UPDATE sizeList 데이터 변경

--- src/data/sizeList.ts
+++ src/data/sizeList.ts
@@ -8,94 +8,146 @@ export interface SizeListType {
 
 export const sizeList: SizeListType[] = [
     {
-        inputId: "searchSize061",
-        inputValue: "061",
-        inputDataText: "061",
-        labelHtmlFor: "searchSize061",
-        text: "061"
+        inputId: "searchSizeXS",
+        inputValue: "XS",
+        inputDataText: "XS",
+        labelHtmlFor: "searchSizeXS",
+        text: "XS"
     },   
     {
-        inputId: "searchSize064",
-        inputValue: "064",
-        inputDataText: "064",
-        labelHtmlFor: "searchSize064",
-        text: "064"
+        inputId: "searchSizeS",
+        inputValue: "S",
+        inputDataText: "S",
+        labelHtmlFor: "searchSizeS",
+        text: "S"
     },   
     {
-        inputId: "searchSize067",
-        inputValue: "067",
-        inputDataText: "067",
-        labelHtmlFor: "searchSize067",
-        text: "067"
+        inputId: "searchSizeM",
+        inputValue: "M",
+        inputDataText: "M",
+        labelHtmlFor: "searchSizeM",
+        text: "M"
     },   
     {
-        inputId: "searchSize070",
-        inputValue: "070",
-        inputDataText: "070",
-        labelHtmlFor: "searchSize070",
-        text: "070"
+        inputId: "searchSizeL",
+        inputValue: "L",
+        inputDataText: "L",
+        labelHtmlFor: "searchSizeL",
+        text: "L"
     },   
     {
-        inputId: "searchSize073",
-        inputValue: "073",
-        inputDataText: "073",
-        labelHtmlFor: "searchSize073",
-        text: "073"
+        inputId: "searchSizeXL",
+        inputValue: "XL",
+        inputDataText: "XL",
+        labelHtmlFor: "searchSizeXL",
+        text: "XL"
     },   
     {
-        inputId: "searchSize076",
-        inputValue: "076",
-        inputDataText: "076",
-        labelHtmlFor: "searchSize076",
-        text: "076"
+        inputId: "searchSizeXXL",
+        inputValue: "XXL",
+        inputDataText: "XXL",
+        labelHtmlFor: "searchSizeXXL",
+        text: "XXL"
     },   
     {
-        inputId: "searchSize080",
-        inputValue: "080",
-        inputDataText: "080",
-        labelHtmlFor: "searchSize080",
-        text: "080"
-    },   
-    {
-        inputId: "searchSize085",
-        inputValue: "085",
-        inputDataText: "085",
-        labelHtmlFor: "searchSize085",
-        text: "085"
-    },   
-    {
-        inputId: "searchSize090",
-        inputValue: "090",
-        inputDataText: "090",
-        labelHtmlFor: "searchSize090",
-        text: "090"
-    },   
-    {
-        inputId: "searchSize095",
-        inputValue: "095",
-        inputDataText: "095",
-        labelHtmlFor: "searchSize095",
-        text: "095"
-    },   
-    {
-        inputId: "searchSize100",
-        inputValue: "100",
-        inputDataText: "100",
-        labelHtmlFor: "searchSize100",
-        text: "100"
-    },   
-    {
-        inputId: "searchSize105",
-        inputValue: "105",
-        inputDataText: "105",
-        labelHtmlFor: "searchSize105",
-        text: "105"
-    },   
-    {
-        inputId: "searchSize110",
-        inputValue: "110",
-        inputDataText: "110",
-        labelHtmlFor: "searchSize110",
-        text: "110"
-    },   
+        inputId: "searchSizeFREE",
+        inputValue: "FREE",
+        inputDataText: "FREE",
+        labelHtmlFor: "searchSizeFREE",
+        text: "FREE"
+    }  
 ]
+
+// export const sizeList: SizeListType[] = [
+//     {
+//         inputId: "searchSize061",
+//         inputValue: "061",
+//         inputDataText: "061",
+//         labelHtmlFor: "searchSize061",
+//         text: "061"
+//     },   
+//     {
+//         inputId: "searchSize064",
+//         inputValue: "064",
+//         inputDataText: "064",
+//         labelHtmlFor: "searchSize064",
+//         text: "064"
+//     },   
+//     {
+//         inputId: "searchSize067",
+//         inputValue: "067",
+//         inputDataText: "067",
+//         labelHtmlFor: "searchSize067",
+//         text: "067"
+//     },   
+//     {
+//         inputId: "searchSize070",
+//         inputValue: "070",
+//         inputDataText: "070",
+//         labelHtmlFor: "searchSize070",
+//         text: "070"
+//     },   
+//     {
+//         inputId: "searchSize073",
+//         inputValue: "073",
+//         inputDataText: "073",
+//         labelHtmlFor: "searchSize073",
+//         text: "073"
+//     },   
+//     {
+//         inputId: "searchSize076",
+//         inputValue: "076",
+//         inputDataText: "076",
+//         labelHtmlFor: "searchSize076",
+//         text: "076"
+//     },   
+//     {
+//         inputId: "searchSize080",
+//         inputValue: "080",
+//         inputDataText: "080",
+//         labelHtmlFor: "searchSize080",
+//         text: "080"
+//     },   
+//     {
+//         inputId: "searchSize085",
+//         inputValue: "085",
+//         inputDataText: "085",
+//         labelHtmlFor: "searchSize085",
+//         text: "085"
+//     },   
+//     {
+//         inputId: "searchSize090",
+//         inputValue: "090",
+//         inputDataText: "090",
+//         labelHtmlFor: "searchSize090",
+//         text: "090"
+//     },   
+//     {
+//         inputId: "searchSize095",
+//         inputValue: "095",
+//         inputDataText: "095",
+//         labelHtmlFor: "searchSize095",
+//         text: "095"
+//     },   
+//     {
+//         inputId: "searchSize100",
+//         inputValue: "100",
+//         inputDataText: "100",
+//         labelHtmlFor: "searchSize100",
+//         text: "100"
+//     },   
+//     {
+//         inputId: "searchSize105",
+//         inputValue: "105",
+//         inputDataText: "105",
+//         labelHtmlFor: "searchSize105",
+//         text: "105"
+//     },   
+//     {
+//         inputId: "searchSize110",
+//         inputValue: "110",
+//         inputDataText: "110",
+//         labelHtmlFor: "searchSize110",
+//         text: "110"
+//     },   
+// ]
